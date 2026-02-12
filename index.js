036abf499a113a1f140533a59164f20b380ca43d
TONY OCHIENG <tonyopuodho@gmail.com>
2026-02-12 06:27:32 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-//chunking arrays
+//chunking array
 
 const chunk = (array,size) => {
     const chunked = []
     let index = 0
-
-    while(index < array.length) {
-        const chunk = array.slice(index, index + size)
-        chunked.push(chunk)
+    while (index < array.length){
+        const chunck = array.slice(index,index + size)
+        chunked.push(chunck)
         index += size
     }
 
     return chunked
 }
 
-console.log(chunk([1,2,3,4,5,6,7,8,9,10],3))
+console.log(chunk([1,2,3,4,5,6,7,8,9,10],2))
