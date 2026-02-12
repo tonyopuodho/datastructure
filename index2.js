53e11f3f2f27d1ca92188f54ae9d67033fc2f779
TONY OCHIENG <tonyopuodho@gmail.com>
2026-02-12 11:06:02 +0300

    pop operation

--- index2.js
+++ index2.js
@@ -16,9 +16,31 @@ class LinkedList{
 
     push(value){
         let newNode = new Node(value)
+         
+        if (!this.head){
+            this.head = newNode
+            this.tail = newNode
+        }
+
         this.tail.next = newNode
         this.tail = newNode
         this.length++
+    }
+
+    pop(){
+        let temp = this.head
+        let prev = this.head
+
+        while(temp.next){
+            prev = temp
+            temp = prev.next
+        }
+
+        this.tail = temp
+        this.tail.next = null
+        this.length--
+
+        return temp
     }
 }
 
