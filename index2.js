dfeed90f03e48873735d68a5fcad3b8f47329557
TONY OCHIENG <tonyopuodho@gmail.com>
2026-02-12 10:46:35 +0300

    push functionality

--- index2.js
+++ index2.js
@@ -13,4 +13,14 @@ class LinkedList{
         this.tail = this.head
         this.length = 1
     }
+
+    push(value){
+        let newNode = new Node(value)
+        this.tail.next = newNode
+        this.tail = newNode
+        this.length++
+    }
 }
+
+const linkedList = new LinkedList(1)
+console.log(linkedList)
